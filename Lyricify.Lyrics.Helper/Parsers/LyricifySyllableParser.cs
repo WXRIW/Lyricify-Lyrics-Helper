1150ae880bfb55b60c2549aa38b67f5cf39ad4ee
XY Wang <wxy11787@foxmail.com>
2023-07-29 11:30:59 +0800

    [Fix] Lyricify Syllable Parser's QRC Background Vocals process

--- Lyricify.Lyrics.Helper/Parsers/LyricifySyllableParser.cs
+++ Lyricify.Lyrics.Helper/Parsers/LyricifySyllableParser.cs
@@ -52,6 +52,23 @@ namespace Lyricify.Lyrics.Parsers
             }
 
             // 将背景人声歌词放入主歌词
+            var newList = SetBackgroundVocalsInfo(list);
+
+            // 应用 Offset
+            if (offset.HasValue && offset.Value != 0)
+            {
+                OffsetHelper.AddOffset(newList, offset.Value);
+            }
+
+            return newList;
+        }
+
+        /// <summary>
+        /// 获取将背景人声歌词放入主歌词后的新列表 (会破坏原列表中的 items)
+        /// </summary>
+        public static List<ILineInfo> SetBackgroundVocalsInfo(List<SyllableLineInfoWithSubLineState> list)
+        {
+            // 将已有背景人声行属性的歌词放入主歌词
             for (int i = 1; i < list.Count; i++)
             {
                 if (list[i].IsBackgroundVocals == true)
@@ -61,20 +78,30 @@ namespace Lyricify.Lyrics.Parsers
                 }
             }
 
-            var returnList = new List<ILineInfo>();
+            // 判断没有预设属性的行
+            bool IsNotBackgroundVocals(SyllableLineInfoWithSubLineState line) => line.IsBackgroundVocals is null && !line.IsBracketedLyrics || line.IsBackgroundVocals == false;
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (list[i].IsBackgroundVocals == null && list[i].IsBracketedLyrics) // 是符合背景人声条件的歌词行
+                {
+                    if (IsNotBackgroundVocals(list[i - 1]) && list[i - 1].SubLine is null) // 上一行符合主歌词行的条件
+                    {
+                        if (i >= list.Count || IsNotBackgroundVocals(list[i + 1])) // 下一行不是背景歌词行
+                        {
+                            list[i - 1].SubLine = SyllableLineInfoWithSubLineState.GetSyllableLineInfo(list[i]);
+                            list.RemoveAt(i--);
+                        }
+                    }
+                }
+            }
 
+            var lines = new List<ILineInfo>();
             // 移除 IsBackgroundVocals 属性
             for (int i = 0; i < list.Count; i++)
             {
-                returnList.Add(SyllableLineInfoWithSubLineState.GetSyllableLineInfo(list[i]));
+                lines.Add(SyllableLineInfoWithSubLineState.GetSyllableLineInfo(list[i]));
             }
-
-            if (offset.HasValue && offset.Value != 0)
-            {
-                OffsetHelper.AddOffset(returnList, offset.Value);
-            }
-
-            return returnList;
+            return lines;
         }
 
         public class SyllableLineInfoWithSubLineState : SyllableLineInfo
@@ -87,6 +114,11 @@ namespace Lyricify.Lyrics.Parsers
             /// 是否是背景人声歌词
             /// </summary>
             public bool? IsBackgroundVocals { get; set; } = null;
+
+            /// <summary>
+            /// 头尾是括号的歌词
+            /// </summary>
+            public bool IsBracketedLyrics => (Text.StartsWith("(") || Text.StartsWith("（")) && (Text.EndsWith(")") || Text.EndsWith("）"));
 
             /// <summary>
             /// 创建一个 SyllableLineInfo 实例，以便与 SyllableLineInfoWithSubLineState 完全分离
