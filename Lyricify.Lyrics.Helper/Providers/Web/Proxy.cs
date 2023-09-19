452ddac5f6be3b5159e5d11540a3941c629b73f9
XY Wang <wxy11787@foxmail.com>
2023-09-19 14:56:52 +0800

    [Add] Proxy support for Providers

---
+++ Lyricify.Lyrics.Helper/Providers/Web/Proxy.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace Lyricify.Lyrics.Providers.Web
+{
+    public static class Proxy
+    {
+        public static void SetProxy(string host, int port, string? username, string? password)
+        {
+            var handler = new HttpClientHandler
+            {
+                Proxy = new WebProxy(host, port)
+            };
+            if (!string.IsNullOrEmpty(username))
+                handler.Proxy.Credentials = new NetworkCredential(username, password);
+
+            BaseApi.HttpClient = new(handler);
+        }
+
+        public static void ClearProxy()
+        {
+            BaseApi.HttpClient = new();
+        }
+    }
+}
