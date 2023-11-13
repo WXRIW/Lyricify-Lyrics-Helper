6092a4e3e6b6a62fa1d7c6e545bfae9d3424adf3
XY Wang <wxy11787@foxmail.com>
2023-11-13 11:35:13 +0800

    [Add] Lyricify Syllable Generator

--- Lyricify.Lyrics.Helper/Generators/LyricifySyllableGenerator.cs
+++ Lyricify.Lyrics.Helper/Generators/LyricifySyllableGenerator.cs
@@ -1,13 +1,15 @@
-﻿using Lyricify.Lyrics.Helpers.General;
-using Lyricify.Lyrics.Models;
+﻿using Lyricify.Lyrics.Models;
 using System.Text;
-using static Lyricify.Lyrics.Parsers.LyricifySyllableParser;
 
 namespace Lyricify.Lyrics.Generators
 {
     public static class LyricifySyllableGenerator
     {
-        // TODO: 仍未完成，不要调用
+        /// <summary>
+        /// 生成 Lyricify Syllable 字符串
+        /// </summary>
+        /// <param name="lyricsData">用于生成的源歌词数据</param>
+        /// <returns>生成出的 Lyricify Syllable 字符串</returns>
         public static string Generate(LyricsData lyricsData)
         {
             if (lyricsData?.Lines is not { Count: > 0 }) return string.Empty;
@@ -16,19 +18,60 @@ namespace Lyricify.Lyrics.Generators
             var lines = lyricsData.Lines;
             for (int i = 0; i < lines.Count; i++)
             {
-                var line = lines[i];
-                AppendLine(sb, line);
-                if (line.SubLine is not null)
-                    AppendLine(sb, line.SubLine);
+                if (lines[i] is SyllableLineInfo line)
+                {
+                    AppendLine(sb, line);
+                    if (line.SubLine is SyllableLineInfo subLine)
+                        AppendLine(sb, subLine, true);
+                }
             }
 
             return sb.ToString();
 
-            static void AppendLine(StringBuilder sb, ILineInfo line)
+            static void AppendLine(StringBuilder sb, SyllableLineInfo line, bool isSubLine = false)
             {
-                if (line.StartTime.HasValue)
+                // 添加属性信息
+                sb.Append('[');
+                sb.Append(isSubLine
+                    ? line.LyricsAlignment switch
+                    {
+                        LyricsAlignment.Left => 7,
+                        LyricsAlignment.Right => 8,
+                        _ => 6,
+                    }
+                    : line.LyricsAlignment switch
+                    {
+                        LyricsAlignment.Left => 4,
+                        LyricsAlignment.Right => 5,
+                        _ => 3,
+                    });
+                sb.Append(']');
+
+                // 添加音节信息
+                foreach(var syllable in line.Syllables)
                 {
-                    //sb.AppendLine($"[{StringHelper.FormatTimeMsToTimestampString(line.StartTime.Value)}]{line.Text}");
+                    if (syllable is SyllableInfo syllableInfo)
+                    {
+                        Append(syllableInfo);
+                    }
+                    else if (syllable is FullSyllableInfo fullSyllableInfo)
+                    {
+                        foreach (var item in fullSyllableInfo.SubItems)
+                        {
+                            Append(item);
+                        }
+                    }
+                }
+                sb.AppendLine();
+
+                void Append(SyllableInfo item)
+                {
+                    sb.Append(item.Text);
+                    sb.Append('(');
+                    sb.Append(item.StartTime);
+                    sb.Append(',');
+                    sb.Append(((ISyllableInfo)item).Duration);
+                    sb.Append(')');
                 }
             }
         }
