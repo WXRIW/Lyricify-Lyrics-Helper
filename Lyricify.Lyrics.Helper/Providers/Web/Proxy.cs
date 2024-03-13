b15b9cf1830e5355a6aa203bec53ddd6b6adad49
XY Wang <wxy11787@foxmail.com>
2024-03-13 10:47:26 +0800

    [Add] Disable proxy method for provider APIs

--- Lyricify.Lyrics.Helper/Providers/Web/Proxy.cs
+++ Lyricify.Lyrics.Helper/Providers/Web/Proxy.cs
@@ -4,6 +4,13 @@ namespace Lyricify.Lyrics.Providers.Web
 {
     public static class Proxy
     {
+        /// <summary>
+        /// 设置代理
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
         public static void SetProxy(string host, int port, string? username, string? password)
         {
             var handler = new HttpClientHandler
@@ -16,6 +23,22 @@ namespace Lyricify.Lyrics.Providers.Web
             BaseApi.HttpClient = new(handler);
         }
 
+        /// <summary>
+        /// 禁用代理，不使用系统代理
+        /// </summary>
+        public static void DisableProxy()
+        {
+            var handler = new HttpClientHandler
+            {
+                Proxy = null,
+                UseProxy = false
+            };
+            BaseApi.HttpClient = new(handler);
+        }
+
+        /// <summary>
+        /// 清除代理设定
+        /// </summary>
         public static void ClearProxy()
         {
             BaseApi.HttpClient = new();
